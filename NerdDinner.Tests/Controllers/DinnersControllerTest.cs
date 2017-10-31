da2e407552e2a13f8f546f4b5927ce8a3c2dab4f
Jeremy Davis <jdavis@paraport.com>
2017-10-31 14:36:47 -0700

    working edit controller tests

--- NerdDinner.Tests/Controllers/DinnersControllerTest.cs
+++ NerdDinner.Tests/Controllers/DinnersControllerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using NerdDinner.Controllers;
 using NerdDinner.Models;
 using NerdDinner.Tests.Fakes;
@@ -52,6 +53,18 @@ namespace NerdDinner.Tests.Controllers
             return new DinnersController(repository);
         }
 
+        DinnersController CreateDinnersControllerAs(string userName)
+        {
+            var mock = new Mock<ControllerContext>();
+            mock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns(userName);
+            mock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            var controller = CreateDinnersController();
+            controller.ControllerContext = mock.Object;
+
+            return controller;
+        }
+
         [TestMethod]
         public void DetailsAction_Should_Return_View_For_ExistingDinner()
         {
@@ -78,6 +91,34 @@ namespace NerdDinner.Tests.Controllers
 
             // Assert
             Assert.AreEqual("NotFound", result.ViewName);
+        }
+
+        [TestMethod]
+        public void EditAction_Should_Return_EditView_When_ValidOwner()
+        {
+
+            // Arrange
+            var controller = CreateDinnersControllerAs("SomeUser");
+
+            // Act
+            var result = controller.Edit(1) as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(DinnerFormViewModel));
+        }
+
+        [TestMethod]
+        public void EditAction_Should_Return_InvalidOwnerView_When_InvalidOwner()
+        {
+
+            // Arrange
+            var controller = CreateDinnersControllerAs("NotOwnerUser");
+
+            // Act
+            var result = controller.Edit(1) as ViewResult;
+
+            // Assert
+            Assert.AreEqual("",result.ViewName);
         }
     }
 }
